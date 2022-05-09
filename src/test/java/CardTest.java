d09aa939ed7f3e29e5551381932433fbbd0c79f0
ZeninAN <95536097+ZeninAN@users.noreply.github.com>
2022-05-09 15:33:35 +0400

    allure

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,6 +1,10 @@
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import data.RegistrationInfo;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -19,6 +23,17 @@ import static java.time.format.DateTimeFormatter.ofPattern;
 import static org.openqa.selenium.Keys.chord;
 
 public class CardTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
 
     @BeforeEach
     public void setUp() {
